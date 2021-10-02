c90334af6d7e4348924587acc414cbdd115b09ce
Dmitry Suzdalev <mail@dimsuz.ru>
2021-10-02 20:00:07 +0200

    Rename source set

--- kmm-desktop-android/build.gradle.kts
+++ kmm-desktop-android/build.gradle.kts
@@ -1,6 +1,5 @@
 buildscript {
-    // __LATEST_COMPOSE_RELEASE_VERSION__
-    val composeVersion = System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "1.0.0-alpha4-build328"
+    val composeVersion = "1.0.0-alpha4-build328"
 
     repositories {
         mavenCentral()
